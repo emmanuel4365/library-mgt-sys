51e9d72b417109c82ab511e4c11b7c84d0ff8b9e
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-10-26 19:35:47 +0100

    ready for deploy

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./routes/book.routes.js";
 import { StatusCodes } from "http-status-codes";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.json());
 
@@ -22,7 +26,7 @@ app.use(function (req, res, next) {
 app.use("/api/v1/books", router);
 
 app.get("/", (req, res) => {
-  res.send("Hello backenders");
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.use("*", (req, res) => {
